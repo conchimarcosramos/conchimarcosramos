1ccfcd537938d012ace4a16b06b510ab56c9f832
conchimarcosramos <conchi.marcos@hotmail.es>
2025-04-22 19:26:22 +0200

    creamos los modelos

--- actividad1/api/models.py
+++ actividad1/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    def __str__(self):
+        return self.nombre
+
+class Gasto(models.Model):
+    descripcion = models.TextField()
+    monto = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha = models.DateTimeField(auto_now_add=True)
+    persona = models.ForeignKey(Persona, on_delete=models.CASCADE, related_name='gastos')
+    def __str__(self):
+        return f"{self.descripcion} - {self.monto}"
+
+class Grupo(models.Model):
+    nombre = models.CharField(max_length=100)
+    personas = models.ManyToManyField(Persona, related_name='grupos')
+    def __str__(self):
+        return self.nombre
